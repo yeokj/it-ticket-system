44d21ebd56d860fd30bb3a38fa1fa0596aa1edf9
Kyame Israel <kyame.israel@gmail.com>
2026-04-02 19:32:59 -0400

    Renamed data members in Ticket class to be more consistent and clear

--- include/models/Ticket.h
+++ include/models/Ticket.h
@@ -6,32 +6,43 @@
 // Class to represent an incident ticket (created by Kyame)
 class Ticket {
 public:
-//constructor
+    // Constructor
     Ticket();
-    Ticket(int ticketNum, std::string clientInfo, std::string techSupport, std::string issue, int dueDate);
-//setters and getters
+    Ticket( int ticketId, 
+            const std::string &clientName, 
+            const std::string &technicianName, 
+            const std::string &issueDescription, 
+            int dueDate,
+            int priorityLevel);
+
+    // Setters and Getters
     void SetTicketNum(int ticketNum);
     int GetTicketNum() const;
-    void SetClientInfo(std::string clientInfo);
+
+    void SetClient(std::string clientInfo);
     std::string GetClientInfo() const;
+
     void SetTechSupport(std::string techSupport);
     std::string GetTechSupport() const;
+
     void SetIssue(std::string issue);
     std::string GetIssue() const;
+
     void SetDueDate(int dueDate);
     int GetDueDate() const;
+
     void SetPriority(int priorityLevel);
     int GetPriority() const;
 
     void Display() const;
 
 private:
-    int ticket;
-    std::string client;
-    std::string technician;
-    std::string issue;
-    int deadline;
-    int priority;
+    int ticketId;
+    const std::string &clientName;
+    const std::string &technicianName;
+    const std::string &issueDescription;
+    int dueDate;
+    int priorityLevel;
 };
 
 #endif
