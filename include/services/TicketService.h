074488426b2277fa9eda6c28405f97e6036355a1
Kyame Israel <kyame.israel@gmail.com>
2026-04-02 23:40:32 -0400

    Moved ticket free functions into TicketService class, grouped by behavior, added comments for clarity

--- include/services/TicketService.h
+++ include/services/TicketService.h
@@ -4,16 +4,23 @@
 #include "Ticket.h"
 #include <vector>
 
-void createTicket(std::vector<Ticket>& tickets);
-void appendTicket(std::vector<Ticket>& tickets);
-void removeTicket(std::vector<Ticket>& tickets);
-void sortTicket(std::vector<Ticket>& tickets);
-void searchTicket(std::vector<Ticket>& tickets);
-void priorityTicket(std::vector<Ticket>& tickets);
-void reverseTicket(std::vector<Ticket>& tickets);
-void displayAllTickets(const std::vector<Ticket>& tickets);
-void updateTicket(std::vector<Ticket>& tickets);
-void filterTicket(const std::vector<Ticket>& tickets);
+class TicketService {
+    public:
+        //CRUD Operations:
+        static void createTicket(std::vector<Ticket>& tickets); // Creates and adds tickets to the ticket vector
+        static void removeTicket(std::vector<Ticket>& tickets); // Removes tickets from the ticket vector
+        static void updateTicket(std::vector<Ticket>& tickets); // Updates ticket in the ticket vector
+
+        // Display and Search:
+        static void displayAllTickets(const std::vector<Ticket>& tickets); // Returns the full ticket vector        
+        static void searchTickets(std::vector<Ticket>& tickets); // Searches for a specific ticket in the ticket vector
+        static void filterTickets(const std::vector<Ticket>& tickets); // Returns tickets based on priority, status, issue, etc
+
+        // Sorting and Ordering:
+        static void sortTickets(std::vector<Ticket>& tickets); // Sorts tickets from lowest to highest priority
+        static void prioritizeTickets(std::vector<Ticket>& tickets); // Sorts tickets from highest to lowest priority
+        static void reverseTickets(std::vector<Ticket>& tickets); // Returns the tickets from new to old
+};
 
 #endif
 
